2d94bdeff4b702a441e910a0e22f0ffdcb47002f
Sanket Giri <sangiri1@publicisgroupe.net>
2019-10-28 20:38:03 +0530

    First Commit

---
+++ src/redux/actions/authorActions.js
@@ -0,0 +1,22 @@
+import * as types from "./actionTypes";
+import * as authorApi from "../../api/authorApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadAuthorSuccess(authors) {
+  return { type: types.LOAD_AUTHORS_SUCCESS, authors };
+}
+
+export function loadAuthors() {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return authorApi
+      .getAuthors()
+      .then(authors => {
+        dispatch(loadAuthorSuccess(authors));
+      })
+      .catch(error => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
